49ba33c220db29e3d8c905bdc579ec6a5a58ceaf
shoaibalirts <shoaibali.rts@gmail.com>
2026-02-03 13:01:18 +0100

    used absolute paths

--- ImageMagick/Program.cs
+++ ImageMagick/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("MagickImage is running!");
+            /*
             using (MagickImage image = new MagickImage(@"assets/mette.jpg"))
             {
                 image.Resize(500, 0); // Resize til bredde 500, bevar aspekt
@@ -15,10 +16,19 @@
                 var readingFolderImage = new MagickImageInfo(@"assets/mette.jpg");
                 Console.WriteLine($"{readingFolderImage.Width} x {readingFolderImage.Height}");
                 Console.WriteLine();
-                // Reading multiple files
-
-
+                // Reading multiple files from a folder
             }
+            */
+            string inputFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\assets");
+            string outputFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\thumbnails");
+
+            List<string> results = Thumbnailer.CreateThumbnails(inputFolder, outputFolder, 150);
+            Console.WriteLine("Thumbnails created:");
+            foreach (var thumb in results) 
+            { 
+                Console.WriteLine(thumb); 
+            }
+
 
             Console.ReadKey();
         }
---
+++ ImageMagick/Thumbnailer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageMagick
+{
+    public class Thumbnailer
+    {
+        public static List<string> CreateThumbnails(string inputFolder, string outputFolder, int width=150)
+        {
+            List<string> thumbnails = new List<string>();
+            foreach(var file in Directory.GetFiles(inputFolder))
+            {
+                var info = new MagickImageInfo(file);
+                Console.WriteLine($"Found {Path.GetFileName(file)} ({info.Width}x{info.Height})");
+                using(var image = new MagickImage(file))
+                {
+                    image.Resize((uint)width, 0);
+                    string outputPath = Path.Combine(outputFolder, Path.GetFileName(file));
+                    image.Write(outputPath);
+                    thumbnails.Add(outputPath);
+                }
+            }
+            return thumbnails;
+        }
+    }
+}
