c204e6b4f8ba431a13f0419999a7035e1608bdc0
shoaibalirts <shoaibali.rts@gmail.com>
2026-02-03 13:14:39 +0100

    Created a new console project called Meme Machine

---
+++ MemeMachine/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MemeMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meme Machine!");
+            Console.ReadKey();
+        }
+    }
+}
