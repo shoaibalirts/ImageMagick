62ec9e22536157433d7e27efd602c81416cc45e4
shoaibalirts <shoaibali.rts@gmail.com>
2026-02-05 11:07:19 +0100

    text is written on the picture

--- MemeMachine/Program.cs
+++ MemeMachine/Program.cs
@@ -1,27 +1,58 @@
-﻿namespace MemeMachine
+﻿using ImageMagick;
+using ImageMagick.Drawing;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MemeMachine
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Meme Machine!");
-            string file1 = "cat1.jpg";
-            string path1 = @"assets";
-            string imageFileFullPath;
-            //imageFileFullPath = Path.GetFullPath(path1);
-            //Console.WriteLine("GetFullPath('{0}') returns '{1}'", path1, imageFileFullPath);
-            imageFileFullPath = Path.GetFullPath(file1);
-            Console.WriteLine("GetFullPath('{0}') returns '{1}'", path1, imageFileFullPath); // path of cat1.jpg
+            string path1 = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\assets\");            //string filePath = Path.Combine(path1,"cat1.jpg");
+            string filePath = Path.Combine(path1, "mette.jpg");
+            Console.WriteLine("Looking for: " + Path.GetFullPath(filePath)); 
+            Console.WriteLine(File.Exists(filePath) ? "Found!" : "Not found!");
+            if (!File.Exists(filePath)) {
+                Console.WriteLine("File not found.");
+                Console.ReadKey();
+                return;
+            }
+            
+
+            string imageFileFullPath = Path.GetFullPath(filePath);
+             Console.WriteLine("{0}'",imageFileFullPath); // path of cat1.jpg
 
             Console.Write("Enter text: ");
             string text = Console.ReadLine();
             Console.WriteLine("You entered: "+text);
-            using (var imageToBeLoaded = new MagicImage(imageFileFullPath))
+            
+            using (MagickImage image = new MagickImage(imageFileFullPath))
             {
-
+                var settings = new Drawables()
+                                    .Font("Arial")
+                                    .FontPointSize(36)
+                                    .FillColor(MagickColors.Red)
+                                    .TextAlignment(TextAlignment.Center); 
+                                settings.Text(100, 100, text); 
+                                image.Draw(settings);
+                                image.Write(@"C:\Users\engrs\Desktop\ProCsharp\ImageMagick\MemeMachine\assets\mette_output.jpg");
             }
             Console.WriteLine("Enter any key to exit the program...");
             Console.ReadKey();
         }
     }
 }
+/*
+image.BackgroundColor = MagickColors.Gray;
+var drawables = new Drawables()
+    .Font("Impact")
+    .FontPointSize(48)
+    .FillColor(MagickColors.Red)
+    .StrokeColor(MagickColors.Black)
+    .StrokeWidth(2)
+    .Gravity(Gravity.South)
+    .Text(0, -30, text);
+
+drawables.Draw(image);
+*/
