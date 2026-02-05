60fcadcc2bf03bce50af90e4da609fe9ab5eb8a5
shoaibalirts <shoaibali.rts@gmail.com>
2026-02-05 09:09:19 +0100

    installed Magick.NET for memes project too

--- MemeMachine/Program.cs
+++ MemeMachine/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Meme Machine!");
+            string file1 = "cat1.jpg";
+            string path1 = @"assets";
+            string imageFileFullPath;
+            //imageFileFullPath = Path.GetFullPath(path1);
+            //Console.WriteLine("GetFullPath('{0}') returns '{1}'", path1, imageFileFullPath);
+            imageFileFullPath = Path.GetFullPath(file1);
+            Console.WriteLine("GetFullPath('{0}') returns '{1}'", path1, imageFileFullPath); // path of cat1.jpg
+
+            Console.Write("Enter text: ");
+            string text = Console.ReadLine();
+            Console.WriteLine("You entered: "+text);
+            using (var imageToBeLoaded = new MagicImage(imageFileFullPath))
+            {
+
+            }
+            Console.WriteLine("Enter any key to exit the program...");
             Console.ReadKey();
         }
     }
